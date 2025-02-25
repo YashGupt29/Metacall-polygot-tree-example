ebaa2f1d506074b910119351f0a2e4606ea20da3
YashGupt29 <yashnitagartala8@gmail.com>
2025-02-26 00:17:15 +0530

    fixes

--- app/root.py
+++
@@ -1,8 +0,0 @@
-from metacall import metacall, metacall_load_from_file
-
-metacall_load_from_file('js', ['./middleNode.js'])
-
-def process_root(node):
-    left = metacall('process_middle', 2 * node)
-    right = metacall('process_middle', 2 * node + 1)
-    return f"Root({node}) " + left + right
